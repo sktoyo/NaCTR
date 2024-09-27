03bf57b8b2fc36003362ef676b19ea28c2358e61
Seunghwan Jung <sktoyo56@gmail.com>
2024-09-27 23:05:02 +0900

    initial commit

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import herb_compound_hit_count as hchc
+import herb_phenotype_hit_count as hphc
+import herb_compound_hit_count_visualization as visual
+import herb_analysis as ha
+
+if __name__ == '__main__':
+    hchc.main()
+    hphc.main()
+    visual.main()
+    ha.main()
